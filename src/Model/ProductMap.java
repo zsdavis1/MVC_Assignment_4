ce963ae42d5218b6e1787df220e69c098614931a
zsdavis1 <zsdavis1@asu.edu>
2026-04-05 13:11:14 -0700

    Add basic functions for products

--- src/Model/ProductMap.java
+++ src/Model/ProductMap.java
@@ -1,4 +1,23 @@
 package Model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductMap {
+    private final Map<Integer, String []> products;
+    private int increment;
+
+    public ProductMap() {
+        products = new HashMap<>();
+        increment = 1;
+    }
+
+    public int getProductCount() {
+        return products.size();
+    }
+
+    public HashMap<Integer, String[]> getProducts() {
+        return new HashMap<>(products);
+    }
+
 }
