9b142f3cc3445597a317ac022cf10e56480132f5
zsdavis1 <zsdavis1@asu.edu>
2026-04-05 13:06:39 -0700

    Create full Product class

---
+++ src/Model/Product.java
@@ -0,0 +1,32 @@
+package Model;
+
+public class Product {
+    private final String name;
+    private final String price;
+    private String quantity;
+
+    public Product(String name, String price, String quantity) {
+        if (name == null || price == null || quantity == null) {
+            throw new IllegalArgumentException("Name, price, or quantity cannot be null");
+        }
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
