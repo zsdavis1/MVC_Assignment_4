89c3a64061aed8670a9ddbb6854ebdccb5a7c83f
zsdavis1 <zsdavis1@asu.edu>
2026-04-05 14:29:22 -0700

    Create basic methods for Sale.java

--- src/Model/Sale.java
+++ src/Model/Sale.java
@@ -1,5 +1,30 @@
 package Model;
 
 public class Sale {
+    private final String customerId;
+    private final String productId;
+    private final int quantity;
+    private double total;
 
+    public Sale (String customerId, String productId, int quantity) {
+        if (customerId == null || customerId.isEmpty() || productId == null || productId.isEmpty() || quantity <= 0) {
+            throw new IllegalArgumentException("Invalid sale data.");
+        }
+
+        this.customerId = customerId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getCustomerName() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
