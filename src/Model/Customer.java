81985d447f23a1551f5ed12212573331337a5fa3
zsdavis1 <zsdavis1@asu.edu>
2026-04-05 12:25:59 -0700

    Create Model package and Customer.java file

---
+++ src/Model/Customer.java
@@ -0,0 +1,26 @@
+package Model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Customer {
+    private final String name;
+    private final String email;
+
+    public Customer(String name, String email) {
+        if (name == null || email == null) {
+            throw new IllegalArgumentException("Name and/or email cannot be null.");
+        }
+
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
