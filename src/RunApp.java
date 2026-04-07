967117e1dc0a54a0b24c7ce2df09d8c82e327059
zsdavis1 <zsdavis1@asu.edu>
2026-04-06 22:54:33 -0700

    Add and create new entry point file to run application

---
+++ src/RunApp.java
@@ -0,0 +1,9 @@
+import View.CornerStoreView;
+
+public class RunApp {
+    public static void main(String[] args) {
+        CornerStoreView entry = new CornerStoreView();
+
+        entry.start();
+    }
+}
