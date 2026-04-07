c3cc1756a81281362ac503420dafa54c0061f1f1
zsdavis1 <zsdavis1@asu.edu>
2026-04-06 21:01:35 -0700

    Create initial constructor and add attributes

--- src/View/CornerStoreView.java
+++ src/View/CornerStoreView.java
@@ -1,4 +1,15 @@
 package View;
 
+import Controller.CornerStoreController;
+
+import java.util.Scanner;
+
 public class CornerStoreView {
+    private final CornerStoreController controller;
+    private final Scanner scanner;
+
+    public CornerStoreView() {
+        this.controller = new CornerStoreController();
+        this.scanner = new Scanner(System.in);
+    }
 }
